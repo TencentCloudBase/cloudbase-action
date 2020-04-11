164653133b32f0db953a4dfa5bf7f82bf5d8b008
Booker Zhao <bookerzhao@tencent.com>
2020-04-11 15:30:42 +0800

    feat(cli): 支持部署

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-const util = require('util')
-const exec = util.promisify(require('child_process').exec)
-const fs = require('fs')
-const path = require('path');
-const writeFile = util.promisify(fs.writeFile)
-const mkdir = util.promisify(fs.mkdir)
+const Client = require('@cloudbase/cli');
 
-try {
+
+const main = async () => {
   // `who-to-greet` input defined in action metadata file
   const secretId = core.getInput('secretId')
   const secretKey = core.getInput('secretKey')
   const envId = core.getInput('envId')
+  const staticSrcPath = core.getInput('staticSrcPath')
+  const staticDestPath = core.getInput('staticDestPath')
+
+  const client = new Client(secretId, secretKey);
+
+  console.log('login success')
+
+  await deployHostingFile(staticSrcPath, staticDestPath, envId)
+
+  console.log('deploy succss')
 
   const time = new Date().toTimeString()
   core.setOutput('time', time)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
-} catch (error) {
-  core.setFailed(error.message)
+}
+
+main().catch(error => core.setFailed(error.message))
+
+
+async function deployHostingFile(srcPath, cloudPath, envId) {
+  const hosting = require('@cloudbase/cli/lib/commands/hosting/hosting')
+
+  return hosting.deploy(
+    {
+      envId
+    },
+    srcPath,
+    cloudPath
+  )
 }
